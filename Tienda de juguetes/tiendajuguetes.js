f3dcf653d296ba4b37bf0a2130d48ca2adf5dc59
AlejandroGB95 <alexdack95@gmail.com>
2025-03-13 19:19:35 +0100

    Tienda de Juguetes
    Esta pagina esta creada con Html5 y Css3 mas un poco de JavaScript. He creado una tienda de juguetes en la que tiene unas cookies que debes aceptar para que te salga la pagina y luego tienes un formulario en la parte derecha por si quieres registrarte. Además tiene un pie de pagina con algunas redes sociales. Cada contenedor contiene (una imagen, un párrafo y un precio con color rojo y tachado).

---
+++ Tienda de juguetes/tiendajuguetes.js
@@ -0,0 +1,8 @@
+function myfuntionaceptar(){
+    document.getElementById("aceptar").style.display="block";
+    document.getElementById("cookies").style.display="none";
+}
+function myfuntioncancelar(){
+    document.getElementById("cancelar").style.display="block";
+    document.getElementById("cookies").style.display="none";
+}
